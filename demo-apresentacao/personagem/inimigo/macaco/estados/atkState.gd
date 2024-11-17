cd180277ed3e74834441952273b6a0a4776a4e14
henriquefac <henrique.fdut@gmail.com>
2024-11-17 18:23:41 -0300

    macaco e novo player

---
+++ demo-apresentacao/personagem/inimigo/macaco/estados/atkState.gd
@@ -0,0 +1,31 @@
+extends State
+class_name AtkState
+
+@export var enemy: macaco
+
+var animationNode: AnimationPlayer
+
+
+func Enter():
+	print("ataque")
+	animationNode = enemy.animationPLayer
+
+	if not animationNode.is_connected("animation_finished",  transitionIdle):
+		animationNode.connect("animation_finished", transitionIdle)
+		
+	enemy.velocity = Vector2()
+	enemy.is_dashing = true
+	animationNode.play("ataque")
+
+func transitionIdle(algo):
+	Transitioned.emit(self, "idle")
+
+func Exit():
+	enemy.is_dashing = false
+	enemy.velocity = Vector2()
+
+func Update(_delta: float):
+	pass
+	
+func Physics_Update(_delta: float):
+	pass
